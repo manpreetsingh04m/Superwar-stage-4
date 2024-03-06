b0428b25228e3d1b884120629e89970a9211b2ff
manpreetsingh04m <ms2404mk@gmail.com>
2024-03-06 09:43:45 +0530

    Done

--- src/app.js
+++ src/app.js
@@ -25,6 +25,11 @@ const PLAYERS = [
 class Player {
   constructor(id, name, type) {
     // Progression 1: Create member variables and assign values
+    this.id = id;
+    this.name = name;
+    this.image = `images/super-${id+1}.png`;
+    this.strength = this.getRandomStrength();
+    this.type = type;
   }
   // getting random strength
   getRandomStrength = () => {
@@ -35,7 +40,18 @@ class Player {
   view = () => {
     // Accumulate HTML template
     // Type your code here
-
+    let player = document.createElement('div');
+    player.classList.add('player');
+    player.setAttribute('data-id', this.id);
+    if (this.selected == true) player.classList.add('selected');
+    let image = document.createElement('img');
+    image.setAttribute('src', this.image);
+    let name = document.createElement('div');
+    name.textContent = this.name;
+    let strength = document.createElement('div');
+    strength.textContent = this.strength;
+    strength.className = 'strength';
+    player.append(image, name, strength);
     return player;
   };
 }
@@ -47,6 +63,11 @@ class Superwar {
     // Create a field players
     // Use Map method to loop through players argument and create new players
     // Type your code here
+    this.players = players.map((player, i) => {
+      let type = i % 2 == 0 ? 'hero' : 'villain';
+      return new Player(i, player, type);
+    });
+    
   }
 
   // Display players in HTML
@@ -73,7 +94,7 @@ class Superwar {
 }
 
 // uncomment this part -- only after you complete progression 3
-// window.onload = () => {
-//     const superwar = new Superwar(PLAYERS);
-//     superwar.viewPlayers();
-// }
+window.onload = () => {
+    const superwar = new Superwar(PLAYERS);
+    superwar.viewPlayers();
+}
